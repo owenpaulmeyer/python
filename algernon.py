e316741f59c1a359417be615c1ccfe95f649ac4a
owenpaulmeyer <owenpaulmeyer@gmail.com>
2020-09-25 23:57:40 -0700

    Update algernon.py

--- algernon.py
+++ algernon.py
@@ -1,2 +1,7 @@
 def substrings(string: str) -> list:
   return [string[i:j] for i in range(len(string)) for j in range(len(string)+1) if i < j]
+
+
+def transpose(mx: list) -> list:
+  size = len(mx[0])
+  return [[row[i] for row in mx] for i in range(size)]
