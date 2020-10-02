c078474cacf39ac864e66547d087165c6b890c38
owenpaulmeyer <owenpaulmeyer@gmail.com>
2020-10-02 15:55:30 -0700

    Update algernon.py

--- algernon.py
+++ algernon.py
@@ -5,3 +5,24 @@ def substrings(string: str) -> list:
 def transpose(mx: list) -> list:
   size = len(mx[0])
   return [[row[i] for row in mx] for i in range(size)]
+
+def merge_lists(a_list, b_list):
+    a_idx = 0
+    b_idx = 0
+    result = []
+    while True:
+        if a_idx == len(a_list):
+            result += b_list[b_idx:]
+            break
+        if b_idx == len(b_list):
+            result += a_list[a_idx:]
+            break
+        a_elem = a_list[a_idx]
+        b_elem = b_list[b_idx]
+        if a_elem < b_elem:
+            result.append(a_elem)
+            a_idx += 1
+        else:
+            result.append(b_elem)
+            b_idx += 1
+    return result
